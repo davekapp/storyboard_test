f9d209f5de028187b87993cc2b2be59221af17d5
David Kapp <davidkapp@coshx.com>
2012-02-02 19:34:10 -0700

    Initial Commit

---
+++ StoryboardTestTests/StoryboardTestTests.h
@@ -0,0 +1,13 @@
+//
+//  StoryboardTestTests.h
+//  StoryboardTestTests
+//
+//  Created by David Kapp on 2/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface StoryboardTestTests : SenTestCase
+
+@end
---
+++ StoryboardTestTests/StoryboardTestTests.m
@@ -0,0 +1,32 @@
+//
+//  StoryboardTestTests.m
+//  StoryboardTestTests
+//
+//  Created by David Kapp on 2/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "StoryboardTestTests.h"
+
+@implementation StoryboardTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in StoryboardTestTests");
+}
+
+@end
